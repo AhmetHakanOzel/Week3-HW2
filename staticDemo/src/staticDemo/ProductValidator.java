15e422b8bccf226eb97e248f142c12283f009fde
AhmetHakanOzel <77487197+AhmetHakanOzel@users.noreply.github.com>
2022-10-20 12:31:26 +0300

    Add files via upload

---
+++ staticDemo/src/staticDemo/ProductValidator.java
@@ -0,0 +1,20 @@
+package staticDemo;
+
+public class ProductValidator {
+	
+	static {
+		System.out.println("Static Yapıcı blok Çalıştı");
+	}
+	
+	public ProductValidator() {
+		System.out.println("Yapıcı blok Çalıştı");
+	}
+	public static boolean isValid(Product product) {
+		if(product.price>0 && !product.name.isEmpty()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
